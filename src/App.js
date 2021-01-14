993bd89fb24a4d1f37e867349ac54c8fa4d78e91
isb <jaredlamont90@gmail.com>
2021-01-14 11:08:58 -0800

    minor: integrated coolvetica font

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ import Footer from './components/Footer'
 
 const App = (props) => {
   return (
-    <div>
+    <div style={{
+      fontFamily: 'coolvetica, sans-serif',
+      fontWeight: 400,
+      fontStyle: 'normal',
+    }}>
       <Nav height={100} />
       <Main height={400} />
       <Footer height={100} />
